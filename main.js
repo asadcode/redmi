8a544d6a086e6fb1702a26053b6bfd11f48ccdd6
Asadbek_7777 <126875859+asadcode@users.noreply.github.com>
2023-03-08 20:36:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var buttn = document.querySelector('.logo');
+var menu = document.querySelector('.big');
+var ikon = document.querySelector('.icon')
+
+buttn.addEventListener('click', function(){
+//   buttn.classList.toggle('again');
+  ikon.classList.toggle('fa-times');
+  menu.classList.toggle('again')
+
+});
